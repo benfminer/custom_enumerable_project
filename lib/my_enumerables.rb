5f626f97b9e4cc56754bd4eb0ad9408d97e7dcd2
benfminer <benfminer@gmail.com>
2024-04-27 07:50:53 -0700

    Completed first two my_enumerables

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,17 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    self.each_with_index do |elem, index|
+      yield(elem, index)
+    end
+  end
+
+  def my_select
+    self.each do |elem|
+      if yield(elem)
+    end
+  end
+
 end
 
 # You will first have to define my_each
@@ -8,4 +20,9 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    self.each do |elem|
+      yield(elem)
+    end
+  end
 end
